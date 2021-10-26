495ef3caa9bf65b42c942ac702f5fcb62a6395f7
Sam <samuelmcdouall@gmail.com>
2021-10-26 21:24:05 +0100

    Update Cave.cs

--- Digital Studio Practice/Assets/Scripts/Cave.cs
+++ Digital Studio Practice/Assets/Scripts/Cave.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class Cave : MonoBehaviour
 {
-    // Start is called before the first frame update
     public GameObject cinematic_camera;
     private void OnCollisionEnter(Collision collision)
     {
