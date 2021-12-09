f5bd5db21b6a302160207e8a7cc513e011d45b60
Sam <samuelmcdouall@gmail.com>
2021-12-09 15:08:20 +0000

    Refactored CinematicArea script

--- Digital Studio Practice/Assets/Scripts/CinematicCamera/CinematicArea.cs
+++ Digital Studio Practice/Assets/Scripts/CinematicCamera/CinematicArea.cs
@@ -13,69 +13,65 @@ public class CinematicArea : MonoBehaviour
     GameObject player_camera;
     SoundManager sound_manager_script;
 
-    private void Start()
+    void Start()
     {
         player_camera = GameObject.FindGameObjectWithTag("MainCamera");
         sound_manager_script = GameObject.FindGameObjectWithTag("SoundManager").GetComponent<SoundManager>();
         played_entrance_audioclip = false;
     }
-
-    private void OnTriggerExit(Collider collider)
+    void OnTriggerExit(Collider collider)
     {
         if (collider.gameObject.tag == "Player")
         {
             float distance_from_player_to_regular_point = Vector3.Distance(collider.gameObject.transform.position, regular_area_point.transform.position);
             float distance_from_player_to_cinematic_point = Vector3.Distance(collider.gameObject.transform.position, cinematic_area_point.transform.position);
-            if (distance_from_player_to_regular_point <= distance_from_player_to_cinematic_point)
-            {
-                CameraManager.DisableAllEnabledCameras();
-                CameraManager.EnableCamera(player_camera);
+            SwitchToCloserCamera(distance_from_player_to_regular_point, distance_from_player_to_cinematic_point);
+        }
+    }
 
-                
-                if (sound_manager_script.player_as.clip.name != sound_manager_script.main_game_music.name)
-                {
-                    //print("playing main music");
-                    sound_manager_script.PlayMainMusic();
-                }
+    void SwitchToCloserCamera(float player_to_regular, float player_to_cinematic)
+    {
+        if (player_to_regular <= player_to_cinematic)
+        {
+            ChangeCameraTo(player_camera);
+
+            if (sound_manager_script.player_as.clip.name != sound_manager_script.main_game_music.name)
+            {
+                sound_manager_script.PlayMainMusic();
+            }
+        }
+        else
+        {
+            Player.last_camera_tr = Camera.main.transform;
+            ChangeCameraTo(cinematic_camera);
+
+            if (!played_entrance_audioclip)
+            {
+                sound_manager_script.PlayAreaClip(entrance_audioclip);
+                played_entrance_audioclip = true;
+                Invoke("PlayAreaClipAfterEntrance", entrance_audioclip.length + 1.0f);
             }
             else
             {
-                Player.last_camera_tr = Camera.main.transform;
-                CameraManager.DisableAllEnabledCameras();
-                CameraManager.EnableCamera(cinematic_camera);
-                if (!played_entrance_audioclip)
+                if (sound_manager_script.player_as.clip.name != area_audioclip.name)
                 {
-                    //print("playing area entrance clip for the first and only time");
-                    sound_manager_script.PlayAreaClip(entrance_audioclip);
-                    played_entrance_audioclip = true;
-                    Invoke("PlayAreaClipAfterEntrance", entrance_audioclip.length + 1.0f);
-                }
-                else
-                {
-                    if (sound_manager_script.player_as.clip.name != area_audioclip.name)
-                    {
-                        //print("playing area ambient sound now on returning to area");
-                        sound_manager_script.PlayAreaClip(area_audioclip);
-                    }
+                    sound_manager_script.PlayAreaClip(area_audioclip);
                 }
             }
         }
+    }
+
+    void ChangeCameraTo(GameObject camera)
+    {
+        CameraManager.DisableAllEnabledCameras();
+        CameraManager.EnableCamera(camera);
     }
 
     void PlayAreaClipAfterEntrance()
     {
         if (sound_manager_script.player_as.clip.name != sound_manager_script.main_game_music.name)
         {
-            //print("playing area ambient sound now after entrance");
             sound_manager_script.PlayAreaClip(area_audioclip);
-        }
-    }
-
-    private void OnTriggerEnter(Collider collider)
-    {
-        if (collider.gameObject.tag == "Player")
-        {
-
         }
     }
 }
