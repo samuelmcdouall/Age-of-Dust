4e94a7349d41b28a58c4278b9e5a21b78b572071
Sam <samuelmcdouall@gmail.com>
2021-12-16 21:45:31 +0000

    Added UI for first build

--- Digital Studio Practice/Assets/Scripts/UI/DisplayUI.cs
+++ Digital Studio Practice/Assets/Scripts/UI/DisplayUI.cs
@@ -11,11 +11,20 @@ public class DisplayUI : MonoBehaviour
     public float fade_in_time;
     public float display_time;
     public float fade_out_time;
+    public bool display_immediately;
     void Start()
     {
         ui_image = GetComponent<Image>();
         ui_image.color = new Color(ui_image.color.r, ui_image.color.g, ui_image.color.b, 0.0f);
-        ui_text.color = new Color(ui_text.color.r, ui_text.color.g, ui_text.color.b, 0.0f);
+        if (ui_text)
+        {
+            ui_text.color = new Color(ui_text.color.r, ui_text.color.g, ui_text.color.b, 0.0f);
+        }
+
+        if (display_immediately)
+        {
+            DisplayAnimatedUI();
+        }
     }
 
     public void DisplayAnimatedUI()
@@ -31,7 +40,10 @@ public class DisplayUI : MonoBehaviour
         {
             float faded_opacity = Mathf.Lerp(0.0f, 1.0f, fade_in_timer / fade_in_time);
             ui_image.color = new Color(ui_image.color.r, ui_image.color.g, ui_image.color.b, faded_opacity);
-            ui_text.color = new Color(ui_text.color.r, ui_text.color.g, ui_text.color.b, faded_opacity);
+            if (ui_text)
+            {
+                ui_text.color = new Color(ui_text.color.r, ui_text.color.g, ui_text.color.b, faded_opacity);
+            }
             fade_in_timer += Time.deltaTime;
             yield return null;
         }
@@ -52,7 +64,10 @@ public class DisplayUI : MonoBehaviour
         {
             float faded_opacity = Mathf.Lerp(1.0f, 0.0f, fade_out_timer / fade_out_time);
             ui_image.color = new Color(ui_image.color.r, ui_image.color.g, ui_image.color.b, faded_opacity);
-            ui_text.color = new Color(ui_text.color.r, ui_text.color.g, ui_text.color.b, faded_opacity);
+            if (ui_text)
+            {
+                ui_text.color = new Color(ui_text.color.r, ui_text.color.g, ui_text.color.b, faded_opacity);
+            }
             fade_out_timer += Time.deltaTime;
             yield return null;
         }
