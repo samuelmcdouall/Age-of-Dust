231212064bc072b9f384528cfc35d6d19ea89a7a
Sam <samuelmcdouall@gmail.com>
2021-12-17 13:14:05 +0000

    Made public for volume settings, anchored some UI in the menu

--- Digital Studio Practice/Assets/Scripts/Managers/VolumeManager.cs
+++ Digital Studio Practice/Assets/Scripts/Managers/VolumeManager.cs
@@ -5,12 +5,14 @@ public class VolumeManager : MonoBehaviour
     public static float sfx_volume;
     public static float music_volume;
     [SerializeField]
-    float default_value;
+    float default_value_sfx_volume;
+    [SerializeField]
+    float default_value_music_volume;
     void Start()
     {
         // todo make another instance of this in main scene
         DontDestroyOnLoad(gameObject);
-        sfx_volume = PlayerPrefs.GetFloat("SFX Volume", default_value);
-        music_volume = PlayerPrefs.GetFloat("Music Volume", default_value);
+        sfx_volume = PlayerPrefs.GetFloat("SFX Volume", default_value_sfx_volume);
+        music_volume = PlayerPrefs.GetFloat("Music Volume", default_value_music_volume);
     }
 }
