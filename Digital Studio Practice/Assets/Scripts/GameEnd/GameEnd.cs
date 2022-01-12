1cb6cb8bc472d0184d7fde2aff38eb9dbe6be481
Sam <samuelmcdouall@gmail.com>
2022-01-12 22:38:25 +0000

    Update GameEnd.cs
    or activation DUHHHH

--- Digital Studio Practice/Assets/Scripts/GameEnd/GameEnd.cs
+++ Digital Studio Practice/Assets/Scripts/GameEnd/GameEnd.cs
@@ -3,14 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class GameEnd : MonoBehaviour
 {
-    [SerializeField]
-    float delay_after_object_activation;
     void Start()
-    {
-        Invoke("LoadMainMenu", delay_after_object_activation);
-    }
-
-    void LoadMainMenu()
     {
         SceneManager.LoadScene("Menu Scene");
     }
